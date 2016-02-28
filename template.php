3441c8de52e963bd0159e418aacb22b8250133bc
Joshua Pangborn <joshua.pangborn@gmail.com>
2016-02-28 16:40:02 -0500

    Adjust layout of template.

--- template.php
+++ template.php
@@ -1,8 +1,7 @@
-Sitemap: <?= url('sitemap.xml'); ?>
-
 User-agent: *
 Disallow: /content/*.txt$
 Disallow: /kirby/
 Disallow: /site/
 Disallow: /panel/
 Disallow: /*.md$
+Sitemap: <?= url('sitemap.xml'); ?>
