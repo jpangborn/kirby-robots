e94dfdd6a1ea71082c9052f6da8a6d42779645ed
Joshua Pangborn <joshua.pangborn@gmail.com>
2017-02-18 00:36:57 -0500

    Update for Kirby CLI

--- robots.php
+++ robots.php
@@ -1,12 +1,10 @@
 <?php
 
-kirby()->routes(array(
-  array(
+  $kirby->set('routes', array(
     'pattern' => 'robots.txt',
     'action'  => function() {
       $robots = tpl::load(__DIR__ . DS . 'template.php', array(), true);
 
       return new Response($robots, 'txt');
     }
-  )
-));
+  ));
